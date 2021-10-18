e3c0fa305ed8ee9be6faf8ad62294d0ef485d71a
Nikolay Ivanko <nivanko@yahoo.com>
2021-10-18 20:34:32 +0300

    Adding upload to Docker Hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,6 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=nivanko/predict
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +14,5 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image tag predict $dockerpath:v1.0.1
+docker push $dockerpath:v1.0.1
